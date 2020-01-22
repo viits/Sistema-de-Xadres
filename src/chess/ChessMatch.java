a160c748bd7c239dfb04b61f6ab4532387ae52be
viits <toyobr@hotmail.com>
2020-01-22 14:18:10 -0300

    Contagem de Movimentos da pecas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -87,7 +87,8 @@ public class ChessMatch {
     }
 
     private Piece makeMove(Position source, Position target) {
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece captured = board.removePiece(target);
         board.placePiece(p, target);
         if (captured != null) {
@@ -98,7 +99,8 @@ public class ChessMatch {
     }
 
     private void undoMove(Position source, Position target, Piece capturedPiece) {
-        Piece p = board.removePiece(target);
+        ChessPiece p =(ChessPiece) board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
 
         if (capturedPiece != null) {
